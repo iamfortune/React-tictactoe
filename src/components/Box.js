243bfefe282f4539348a1bb464dd2d4bd9a15334
iamfortune <ikechifortune@gmail.com>
2020-09-14 16:07:37 +0100

    added files to git

---
+++ src/components/Box.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ 
+
+function Box({ value, onClick }) {
+    return (
+        <button
+            onClick={onClick}>
+            {value}
+        </button>
+    ); 
+}
+
+export default Box
